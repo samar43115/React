935e7d6a14adaced3f9ec0ef531010d91d0c8307
explo65 <142860150+samar43115@users.noreply.github.com>
2024-11-07 10:45:05 +0000

    behind the scene

--- 01ViteReact/src/main.jsx
+++ 01ViteReact/src/main.jsx
@@ -1,8 +1,28 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+
+const reactelement ={
+    type:'a',
+    props:{
+        href:'https://www.notion.so/',
+        target:'_blank'
+    },
+    children:'Click this link to visit Notion'
+}
+
+const reactElement = React.createElement(
+    'a',
+    {href:'https://www.notion.so/', target:'_blank'},
+    'Click Me'
+)
+
+const randomElement =(
+    <h1>Hellow there</h1>
+)
 
 createRoot(document.getElementById('root')).render(
-  
-    App()
-  
+    // App()
+    //   randomElement -> </randomElement> won't work even if you make 'r' capital as it is same as randomElement()
+    // reactelement -> won't work as react transpiler has its own defention of creating an element. 
+    reactElement
 )
